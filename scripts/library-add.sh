9e963da0d5e30947dbf550c0dd5a88ba091be720
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-03-13 23:07:45 +0100

    Made all containers base read-only, have them run as not root

--- scripts/library-add.sh
+++ scripts/library-add.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec --workdir /usr/src/stewdio-api api python -m stewdio.library add "$@"
+docker-compose exec --user api --workdir /usr/src/stewdio-api api python -m stewdio.library add "$@"
